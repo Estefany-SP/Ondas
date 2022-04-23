b66d423301bcaa72c351aa435dc6d5ff0362b223
Estefany-SP <104262245+Estefany-SP@users.noreply.github.com>
2022-04-23 12:23:21 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import nuumpy as np
+import matplotlib.pyplot as plt
+st.sidebar.title("Software Educativo para el estudio de fenomenos ondulatorios")
+op1=st.sidebar.radio("",["Ondas Estacionarias","Interderencia de ondas","Onda general"])
+if op1=="Ondas Estacionarias":
+  st.title(op1)
+  n=st.slider("",1,10)
+  x=np.linspace(0,2*np.pi,150)
+#st.write(x)
+  y=np.sin(n*x/2)
+  fig, ax = plt.subplots.()
+  st.pyplot(fig)
